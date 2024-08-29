0497b295e0fa5d69c33ac5ea42bf327944ec5207
Doctorwho07 <alexis.rouches@icloud.com>
2024-08-29 21:02:52 +0200

    update content and style

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => (
   <div className="container mt-5">
     <div className="jumbotron text-center p-5 mb-5 image-bg">
-      <h1 className="display-4 font-weight-bold">Bienvenue sur mon blog</h1>
+      <h1 className="display-4 font-weight-bold">Bienvenue sur mon Blog</h1>
       <p className="lead">
         Je suis Alexis ROUCHES, étudiant en Cybersécurité à Lyon Ynov Campus.{" "}
         <br />
--- src/pages/Portfolio/Competences.jsx
+++ src/pages/Portfolio/Competences.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/competences.css";
 
 const Competences = () => (
   <div className="container mt-5" id="competences">
@@ -12,102 +13,117 @@ const Competences = () => (
           <div className="card-body">
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <strong>OS:</strong> Linux Kali, Windows Serveur, MacOS
+                <strong>Operating Systems:</strong> Linux Kali, Ubuntu, Windows
+                10/11, Windows Server, MacOS
               </li>
               <li className="list-group-item">
-                <strong>Network:</strong> Nmap, Burp, Wireshark, Cisco, OpenVPN
+                <strong>Network & Security:</strong> Nmap, Burp Suite,
+                Wireshark, Cisco, OpenVPN, pfSense, Firewall configuration,
+                Network troubleshooting
               </li>
               <li className="list-group-item">
-                <strong>Languages:</strong> Python, Java, C#, JS, PHP, SQL,
-                HTML, CSS, Flutter, Vue, NodeJS
+                <strong>Programming Languages:</strong> Python, Java, C#,
+                JavaScript, PHP, SQL, HTML, CSS, Flutter, Vue.js, Node.js, Shell
+                scripting
               </li>
               <li className="list-group-item">
-                <strong>Tools:</strong> Anaconda, Docker, Parallels, Cisco
-                Packet Tracer, Linux
+                <strong>Development Tools:</strong> Anaconda, Docker, Git,
+                GitHub, Parallels, Cisco Packet Tracer, Visual Studio Code
+              </li>
+              <li className="list-group-item">
+                <strong>System Administration:</strong> Windows Server setup,
+                Active Directory management, Virtualization (VMware, VirtualBox,
+                Hyper-V), switch management
+              </li>
+              <li className="list-group-item">
+                <strong>Database Management:</strong> MySQL, PostgreSQL, SQL
+                Server, SSMS, Data backup and recovery
+              </li>
+              <li className="list-group-item">
+                <strong>Cloud Technologies:</strong>Google Cloud Platform,
+                Microsoft OneDrive
+              </li>
+              <li className="list-group-item">
+                <strong>Hardware Maintenance:</strong> Server maintenance, Disk
+                health monitoring (S.M.A.R.T), RAM upgrade, Hardware replacement
               </li>
             </ul>
           </div>
         </div>
       </div>
 
-      <div className="col-md-6 d-flex align-items-stretch">
+      <div className="col-md-6 d-flex align-items-stretch" id="soft">
         <div className="card flex-fill custom-card">
           <div className="card-header">
             <h2 className="card-title mb-0">Soft Skills</h2>
           </div>
           <div className="card-body">
             <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <strong>Esprit critique</strong>
+              <li
+                className="list-group-item"
+                data-author="DI RIENZO Antoine"
+                data-entreprise="CIMRA INFORMATIQUE SERVICES"
+              >
+                <strong>Esprit critique</strong> - Alexis a montré une capacité
+                remarquable à comprendre rapidement les enjeux techniques.
               </li>
-              <li className="list-group-item">
-                <strong>Attitude positive</strong>
+              <li
+                className="list-group-item"
+                data-author="Anthony Pereira"
+                data-entreprise="McDonald's Chasse-sur-Rhône"
+              >
+                <strong>Attitude positive</strong> - Toujours souriant et prêt à
+                aider, il a su s’intégrer rapidement au sein des équipes.
               </li>
-              <li className="list-group-item">
-                <strong>Organisation & méthodologie</strong>
+              <li
+                className="list-group-item"
+                data-author="Anthony Pereira"
+                data-entreprise="McDonald's Chasse-sur-Rhône"
+              >
+                <strong>Organisation & méthodologie</strong> - Alexis a su gérer
+                ses tâches avec rigueur et efficacité, tout en jonglant avec ses
+                études.
               </li>
-              <li className="list-group-item">
-                <strong>Professionnalisme</strong>
+              <li
+                className="list-group-item"
+                data-author="Anthony Pereira"
+                data-entreprise="McDonald's Chasse-sur-Rhône"
+              >
+                <strong>Professionnalisme</strong> - Sa capacité à rester calme
+                sous pression et à offrir un service attentionné a été largement
+                reconnue.
               </li>
-              <li className="list-group-item">
-                <strong>Sens du collectif</strong>
+              <li
+                className="list-group-item"
+                data-author="DI RIENZO Antoine"
+                data-entreprise="CIMRA INFORMATIQUE SERVICES"
+              >
+                <strong>Sens du collectif</strong> - Son excellent esprit
+                d’équipe et son sens de l’initiative ont été très appréciés par
+                ses collègues.
+              </li>
+              <li
+                className="list-group-item"
+                data-author="Xavier Guillermin"
+                data-entreprise="BTX Cases"
+              >
+                <strong>Curiosité</strong> - Alexis est constamment motivé pour
+                apprendre et améliorer ses compétences.
+              </li>
+              <li
+                className="list-group-item"
+                data-author="Anthony Pereira"
+                data-entreprise="McDonald's Chasse-sur-Rhône"
+              >
+                <strong>Sens des responsabilités</strong> - Il a montré une
+                grande capacité à prendre des initiatives et à assumer des
+                responsabilités supplémentaires.
               </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
-    {/* <div className="col-md-6 d-flex align-items-stretch">
-        <div className="card flex-fill custom-card">
-          <div className="card-header">
-            <h2 className="card-title mb-0">Expériences Professionnelles</h2>
-          </div>
-          <div className="card-body">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <strong>Hôte Principal:</strong> McDonald's – Chasse sur Rhône
-                (2023 - 2024)
-                <p>Responsable satisfaction et bien-être client</p>
-              </li>
-              <li className="list-group-item">
-                <strong>Animateur BAFA:</strong> Montélimar – Lyon (2021 - 2022)
-                <p>
-                  Responsable de créer et animer des activités ludiques et
-                  éducatives pour les enfants
-                </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div> */}
-
-    {/* <div className="row mb-5">
-      <div className="col-md-12 d-flex align-items-stretch">
-        <div className="card flex-fill custom-card">
-          <div className="card-header">
-            <h2 className="card-title mb-0">Projets Réalisés</h2>
-          </div>
-          <div className="card-body">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">Volontaide (2023-2024)</li>
-              <li className="list-group-item">Troc-Mon-Toit (2023-2024)</li>
-              <li className="list-group-item">TechnoTalk (2023)</li>
-              <li className="list-group-item">Swopy-fun (2023)</li>
-              <li className="list-group-item">Hangman Web (2023)</li>
-              <li className="list-group-item">Word-search-solver (2023)</li>
-              <li className="list-group-item">Cookie Clicker (2023)</li>
-              <li className="list-group-item">Ydays (2022 - 2023)</li>
-              <li className="list-group-item">Quad (2022)</li>
-              <li className="list-group-item">Sortable (2023)</li>
-              <li className="list-group-item">Boxxle (2023)</li>
-              <li className="list-group-item">Hangman Classique (2023)</li>
-              <li className="list-group-item">Groupie tracker (2023)</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div> */}
   </div>
 );
 
