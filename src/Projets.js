46e7a9ed41f016cd1881e9287288fa1b0f9f062a
Docotrwho07 <alexis.rouches@icloud. com>
2024-09-03 13:39:52 +0200

    update projects path

--- src/Projets.js
+++ src/Projets.js
@@ -2,7 +2,7 @@ const projets = [
   {
     id: 0,
     theme: "Développement Web",
-    image: "/assets/projets_screenshot/volontaide.jpg",
+    image: "/projets_screenshot/volontaide.jpg",
     posts: [
       {
         nom: "Volontaide",
@@ -12,7 +12,7 @@ const projets = [
         description:
           "Volontaide est une application qui permet aux utilisateurs de trouver des opportunités de bénévolat ou d'aide dans leur communauté. Elle connecte les associations et les particuliers avec des volontaires motivés.",
         url: "",
-        screenShot: "./assets/projets_screenshot/vav.jpg",
+        screenShot: "/projets_screenshot/vav.jpg",
         techno: "",
         competences: "",
       },
@@ -24,24 +24,14 @@ const projets = [
         description:
           "Troc-Mon-Toit est une plateforme qui permet de rechercher et de réserver des logements. Elle offre des filtres avancés pour trouver le logement idéal, de l'appartement à l'igloo.",
         url: "https://github.com/B2-Info-23-24/php-Doctorwho07",
-        screenShot: "./assets/projets_screenshot/trocmontoit.png",
-      },
-      {
-        nom: "Création de clés bootables Win 10 / 11 / srv 2022",
-        annee: "2024",
-        brief:
-          "Configuration de clés bootables pour différents systèmes d'exploitation.",
-        description:
-          "Configuration de clés bootables pour Windows 10, 11 et Windows Server 2022, afin de préparer les installations sur plusieurs PC.",
-        url: "",
-        screenShot: "/work/cle-usb.png",
+        screenShot: "/projets_screenshot/trocmontoit.png",
       },
     ],
   },
   {
     id: 1,
     theme: "Jeux Vidéo",
-    image: "/assets/projets_screenshot/hangman-web.png",
+    image: "/projets_screenshot/hangman-web.png",
     posts: [
       {
         nom: "Hangman Web",
@@ -50,7 +40,7 @@ const projets = [
         description:
           "Un jeu du pendu accessible depuis un navigateur web. Simple et amusant, il reprend le concept classique du pendu.",
         url: "https://github.com/Doctorwho07/Hangman-web",
-        screenShot: "./assets/projets_screenshot/hangman-web.png",
+        screenShot: "/projets_screenshot/hangman-web.png",
       },
       {
         nom: "Cookie Clicker",
@@ -59,14 +49,14 @@ const projets = [
         description:
           "Petit jeu de clicker où l'objectif est de cliquer sur des cookies pour accumuler des points.",
         url: "",
-        screenShot: "./assets/projets_screenshot/cookie.png",
+        screenShot: "/projets_screenshot/cookie.png",
       },
     ],
   },
   {
     id: 2,
     theme: "Applications Diverses",
-    image: "/assets/projets_screenshot/ydays.png",
+    image: "/projets_screenshot/ydays.png",
     posts: [
       {
         nom: "Ydays",
@@ -75,7 +65,7 @@ const projets = [
         description:
           "Le site vitrine permet de présenter toutes les figurines réalisées depuis la création du projet et jusqu'à sa fin.",
         url: "https://github.com/Gougouli69/YDetailedFigures",
-        screenShot: "./assets/projets_screenshot/ydays.png",
+        screenShot: "/projets_screenshot/ydays.png",
       },
       {
         nom: "Groupie tracker",
@@ -84,7 +74,7 @@ const projets = [
         description:
           "Le projet Groupie Tracker permet de découvrir de nouveaux artistes grâce à une base de données musicale.",
         url: "",
-        screenShot: "./assets/projets_screenshot/groupie-tracker.png",
+        screenShot: "/projets_screenshot/groupie-tracker.png",
       },
     ],
   },
